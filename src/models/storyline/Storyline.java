8fe149154800e4376d35acf4705ea46b00e2a9e3
kat323 <36419477+kat323@users.noreply.github.com>
2018-06-11 13:09:40 -0400

    Update Storyline.java

--- src/models/storyline/Storyline.java
+++ src/models/storyline/Storyline.java
@@ -1,5 +1,8 @@
 package models.storyline;
 
+import helpers.TextOutput;
+import models.Player;
+
 import java.util.ArrayList;
 
 public class Storyline {
@@ -8,6 +11,13 @@ public class Storyline {
 
     public static boolean nextQuest() {
         // if currQuest puzzle is completed (quest.dialogue.decision.puzzle.isCompleted())
+        if (quests.get(currQuest).dialogue.decision.puzzle.isCompleted()) {
+            increaseLevel();
+            if (currQuest >= quests.size()) {
+                storyFinished();
+                return false;
+            }
+        }
         // then increaseLevel();
         // if currQuest >= quests
         // then storyFinished and return false;
@@ -15,17 +25,21 @@ public class Storyline {
     }
     public static void startQuest() {
         // set current quest to 0
-
+        currQuest = 0;
     }
     public static void storyFinished() {
         // output Thanks for playing JDialogue
+        TextOutput.s = "Thanks for playing!!!!!!!";
         // set quests currQuests to 0
-        // new Player();
+        startQuest();
+        new Player();
     }
     public static void increaseLevel() {
-        // currQuest++();
+        currQuest++;
         // if done a certain num of quests then increase grade
-        //
+        if (currQuest == 1) {
+            Player.grade = 12;
+        }
     }
 
 }
